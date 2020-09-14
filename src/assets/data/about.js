f0b5111a240c2727783bc2758b9304b7dd486111
dhigley6 <dhigley@stanford.edu>
2020-09-14 11:00:45 -0700

    Updated about.js

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -1,7 +1,8 @@
 const name = 'Daniel J Higley';
 const greetingText = 'Hello, I\'m Daniel Higley.';
-const greetingSubtext = 'I\'m interested in machine learning, applied physics, software and computers.';
-const about = `I'm a researcher with experience applying machine learning in applied physics and more than ten years of data analytics experience.`;
+const greetingSubtext = 'I\'m an applied physics PhD with more than 10 years of data analysis experience.';
+const about = `I'm a Stanford applied physics PhD graduate with experience in Python, machine learning techniques, extracting insights from large and complex data sets with statistical data analysis, custom numerical simulations and working cross-functionally.
+I also have domain knowledge in optics, X-ray technology, imaging, computer hardware and materials.`;
 
 const education = [
   {
